1e6c2c4a1e9378603434cf65c664135e9f0f76d1
Sofa <sonya.shkur@bk.ru>
2022-02-03 21:27:00 +0300

    finished project

--- script.js
+++ script.js
@@ -9,15 +9,5 @@ setInterval(function(){
 }, 5000);
 
 
-// Display Mobile Menu
-const menu = document.querySelector(`#mobile-menu`);
-const menuLinks = document.querySelector(`.navbar__menu`);
-
-const mobileMenu = () => {
-    menu.classList.toggle(`is-active`)
-    menuLinks.classList.toggle(`active`)
-}
-
-menu.addEventListener(`click`, mobileMenu);
 
 
